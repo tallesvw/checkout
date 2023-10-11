54581473b9d2514dea06aa4636d4e6d4577a3971
tallesvw <Tallizim_vw@hotmail.com>
2023-10-10 21:47:31 -0300

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("showCreditCard").addEventListener("click", function() {
+    document.querySelector(".credit-card").style.display = "block";
+    document.querySelector(".debit-card").style.display = "none";
+});
+
+document.getElementById("showDebitCard").addEventListener("click", function() {
+    document.querySelector(".debit-card").style.display = "block";
+    document.querySelector(".credit-card").style.display = "none";
+});

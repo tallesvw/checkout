13923edf6a38fe3d3fdbfefebba9a2e1ff93edcc
Talles Lima <tallizim_vw@hotmail.com>
2023-10-11 17:47:53 -0300

    .

--- script.js
+++ script.js
@@ -7,3 +7,5 @@ document.getElementById("showDebitCard").addEventListener("click", function() {
     document.querySelector(".debit-card").style.display = "block";
     document.querySelector(".credit-card").style.display = "none";
 });
+
+document.getElementById("")

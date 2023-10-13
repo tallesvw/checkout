0902046a2fa7d5f92e4ec8c8a1774e9b05f0f8f0
Talles Lima <tallizim_vw@hotmail.com>
2023-10-13 16:51:37 -0300

    .

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ document.getElementById("showDebitCard").addEventListener("click", function() {
 });
 
 document.getElementById("")
+
+
+
+
+function preencherEndereco() {
+    var cep = document.getElementById('cep').value;
+    if (cep.length === 8) { // Verifica se o CEP tem 8 dígitos
+
+        // Faz a solicitação à API ViaCEP
+        fetch(`https://viacep.com.br/ws/${cep}/json/`)
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('rua').value = data.logradouro;
+                document.getElementById('bairro').value = data.bairro;
+                // Preencha outros campos do endereço aqui, se necessário
+            })
+            .catch(error => console.error('Erro ao buscar o CEP: ' + error));
+    }
+}
